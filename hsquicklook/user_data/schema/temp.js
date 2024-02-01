c46a8ade75abda1d0a477704ea2de05cd2a841be
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2024-02-01 13:56:51 +0900

    change schema

--- hsquicklook/user_data/schema/temp.js
+++ hsquicklook/user_data/schema/temp.js
@@ -18,26 +18,12 @@ HSQuickLook.main.schema =
       "tableName": "GL840",
       "contents": {
         "Outer_Vacuum": { "source": "Ch1", "conversion": conversion_PKR251, "type": "float", "format": "%-5.2e Pa", "status": function (v) { return status_func("Ch1", v); } },
-        // "Pressure_inner_MPT200": { "source": "Ch2", "conversion": conversion_MPT200AR, "type": "float", "format": "%-5.2e Pa", "status": function (v) {return status_func("Ch2",v); }},
         "Inner_Pressure": { "source": "Ch3", "type": "float", "format": "%.3f Bar", "status": function (v) { return status_func("Ch3", v); } },
         "LAr_Level": { "source": "Ch4", "type": "float", "format": "%.2f cm", "status": function (v) { return status_func("Ch4", v); } },
         "Oxygen": { "source": "Ch5", "conversion": conversion_OX600, "type": "float", "format": "%-5.1f &#037;", "status": function (v) { return status_func("Ch5", v); } },
         "Room_Tempareture": { "source": "Ch21", "type": "float", "format": "%.3f &#8451", },
         "Humidity": { "source": "Ch22", "type": "float", "format": "%.3f ％" },
         "Dew_Point": { "source": "Ch23", "type": "float", "format": "%.3f &#8451", "conversion": function (v) { dewpoint = v; return v; } },
-        // "BottomT-DewPoint":{"source": "Ch21","type":"float","format":"%.3f &#8451","conversion": function(v){return Bottom_tempareture-dewpoint;},"status": function (v){return status_func("tempareture_warning",Bottom_tempareture-dewpoint)}}
-        // "Ch9": { "source": "Ch9","type": "string", "conversion": convert_string, "format": "%.3f V" },
-        // "Ch10": { "source": "Ch10","type": "string", "conversion": convert_string, "format": "%.3f V" },
-        // "Bottom_temp": { "source": "Ch11", "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func("Ch11",v); } },
-        // "TPB_temp": { "source": "Ch12",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func("Ch12",v); }},
-        // "z15cm_temp": { "source": "Ch13",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func("Ch13",v); }},
-        // "z16_5cm_temp": { "source": "Ch14",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func("Ch14",v); }},
-        // "upper_Baffle_Bottom_temp": { "source": "Ch15", "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func("Ch15",v); }},
-        // "Temperature_16": { "source": "Ch16",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return "test"}},
-        // "Temperature_17": { "source": "Ch17", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_18": { "source": "Ch18", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_19": { "source": "Ch19", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_20": { "source": "Ch20", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
       }
     },
 
@@ -55,15 +41,6 @@ HSQuickLook.main.schema =
         "Below_Anode": { "source": "Ch13", "type": "float", "format": "%.3f &#8451;", "status": function (v) { Below_tempareture = v; return status_func_temp("Ch13", v); } },
         "SiPM": { "source": "Ch12", "type": "float", "format": "%.3f &#8451;", "status": function (v) { SiPM_tempareture = v; return status_func_temp("Ch12", v); } },
         "Bottom_Heater": { "source": "Ch11", "type": "float", "format": "%.3f &#8451;", "status": function (v) { Bottom_tempareture = v; return status_func_temp("Ch11", v); } },
-        // "MPPC_window": { "source": "Ch12",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch12",v); }},
-        // "z15cm": { "source": "Ch13",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch13",v); }},
-        // "z16_5cm": { "source": "Ch14",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch14",v); }},
-        // "upper_Baffle_Bottom": { "source": "Ch15", "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch15",v); }},
-        // // "Temperature_16": { "source": "Ch16",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return "test"}},
-        // // "Temperature_17": { "source": "Ch17", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_18": { "source": "Ch18", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_19": { "source": "Ch19", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_20": { "source": "Ch20", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
       }
     },
     {
@@ -80,15 +57,6 @@ HSQuickLook.main.schema =
         "BelowAnodeT-DewPoint": { "source": "Ch21", "type": "float", "format": "%.3f &#8451", "conversion": function (v) { return Below_tempareture - dewpoint; }, "status": function (v) { return status_func("tempareture_warning", Below_tempareture - dewpoint) } },
         "SiPMT-DewPoint": { "source": "Ch21", "type": "float", "format": "%.3f &#8451", "conversion": function (v) { return SiPM_tempareture - dewpoint; }, "status": function (v) { return status_func("tempareture_warning", SiPM_tempareture - dewpoint) } },
         "BottomT-DewPoint": { "source": "Ch21", "type": "float", "format": "%.3f &#8451", "conversion": function (v) { return Bottom_tempareture - dewpoint; }, "status": function (v) { return status_func("tempareture_warning", Bottom_tempareture - dewpoint) } }
-        // "MPPC_window": { "source": "Ch12",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch12",v); }},
-        // "z15cm": { "source": "Ch13",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch13",v); }},
-        // "z16_5cm": { "source": "Ch14",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch14",v); }},
-        // "upper_Baffle_Bottom": { "source": "Ch15", "type": "float", "format": "%.3f &#8451;", "status": function (v) {return status_func_temp("Ch15",v); }},
-        // // "Temperature_16": { "source": "Ch16",  "type": "float", "format": "%.3f &#8451;", "status": function (v) {return "test"}},
-        // // "Temperature_17": { "source": "Ch17", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_18": { "source": "Ch18", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_19": { "source": "Ch19", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
-        // "Temperature_20": { "source": "Ch20", "conversion": convert_string, "type": "string", "format": "%.3f &#8451;"},
       }
     },
 
@@ -131,29 +99,6 @@ HSQuickLook.main.schema =
         },
       }
     },
-    // {
-    //   "collection": "GL840",
-    //   "directory": "GL840",
-    //   "document": "GL840",
-    //   "period": 60,
-    //   "tableName": "Presssure_log",
-    //   "section": "GL840",
-    //   "contents": {
-    //       "Outer_log": { "type": "trend-graph",
-    //           "group": [
-    //               {"source": "Ch1","conversion":conversion_PKR251_log, "options":{"legend": "Outer_PKR","color": "red"}},
-    //           ],
-    //           "options":{"xWidth": 1000,"yRange":[-4, 5]}
-    //       },
-    // "Inner_log": { "type": "trend-graph",
-    //     "group": [
-    //         {"source": "Ch2", "conversion":conversion_MPT200AR_log, "options":{"legend": "Inner_MPT","color": "blue"}},
-    //     ],
-    //     "options":{"xWidth": 1000,"yRange":[-4, 5]}
-    // },
-    //   }
-
-    // },
     {
       "collection": "GL840",
       "directory": "GL840",
@@ -162,21 +107,21 @@ HSQuickLook.main.schema =
       "tableName": "Presssure",
       "section": "GL840",
       "contents": {
-        "Outer_Pa": {
+        "Outer_Vacuum": {
           "type": "trend-graph",
           "group": [
             { "source": "Ch1", "conversion": conversion_PKR251, "options": { "legend": "Outer_PKR", "color": "red" } },
           ],
           "options": { "xWidth": 10000, "yRange": [0, 0.05], "frame": { "height": 270, "width": 480 } }
         },
-        "Inner_Vacuum_Pa": {
+        "Inner_Vacuum": {
           "type": "trend-graph",
           "group": [
             { "source": "Ch2", "conversion": conversion_MPT200AR, "options": { "legend": "Inner_MPT", "color": "blue" } },
           ],
           "options": { "xWidth": 10000, "yRange": [0, 10.0], "frame": { "height": 270, "width": 480 } }
         },
-        "Inner_Bar": {
+        "Inner_Pressure": {
           "type": "trend-graph",
           "group": [
             { "source": "Ch3", "options": { "legend": "Inner_APR", "color": "green" } },
@@ -184,9 +129,7 @@ HSQuickLook.main.schema =
           "options": { "xWidth": 10000, "yRange": [0.5, 3.0], "frame": { "height": 270, "width": 480 } }
         }
       }
-
     },
-
   ];
 
 
