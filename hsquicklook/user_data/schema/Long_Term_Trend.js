c3f600214aa5dba14250bf4bc5f9ff733ef8421e
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2024-06-12 22:45:34 +0900

    Run2 version

--- hsquicklook/user_data/schema/Long_Term_Trend.js
+++ hsquicklook/user_data/schema/Long_Term_Trend.js
@@ -15,6 +15,7 @@ HSQuickLook.main.schema =
         "Outer_Vacuum": { "source": "Ch1", "conversion": conversion_PKR251, "type": "float", "format": "%-5.2e Pa", "status": function (v) { return status_func("Ch1", v); } },
         "Inner_Pressure": { "source": "Ch3", "type": "float", "format": "%.3f Bar", "status": function (v) { return status_func("Ch3", v); } },
         "LAr_Level": { "source": "Ch4", "type": "float", "format": "%.2f cm", "status": function (v) { return status_func("Ch4", v); } },
+        "TPC_HV": { "source": "Ch7", "type": "float", "format": "%.3f kV", "status": function (v) { return status_func_HV("Ch7", v); }},
         "Oxygen": { "source": "Ch5", "conversion": conversion_OX600, "type": "float", "format": "%-5.1f &#037;", "status": function (v) { return status_func("Ch5", v); } },
         "Room_Tempareture": { "source": "Ch21", "type": "float", "format": "%.3f &#8451", },
         "Humidity": { "source": "Ch22", "type": "float", "format": "%.3f ％" },
@@ -32,6 +33,7 @@ HSQuickLook.main.schema =
       "contents": {
         "Top_Baffle": { "source": "Ch16", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch16", v); } },
         "TPC_Baffle": { "source": "Ch15", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch15", v); } },
+        "FEC_Heater": { "source": "Ch20", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func("Ch20", v); } },
         "Above_Anode": { "source": "Ch14", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch14", v); } },
         "Below_Anode": { "source": "Ch13", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch13", v); } },
         "SiPM": { "source": "Ch12", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch12", v); } },
@@ -53,7 +55,7 @@ HSQuickLook.main.schema =
             { "source": "Ch5", "conversion": conversion_OX600, "options": { "legend": "O2", "color": "red" } }
           ],
           "options": {
-            "xWidth": 10000, "yRange": [0.0, 30], "frame": { "height": 270, "width": 480 }
+            "xWidth": 3600, "yRange": [0.0, 30], "frame": { "height": 270, "width": 480 }
           },
         },
         "Temperature": {
@@ -62,19 +64,20 @@ HSQuickLook.main.schema =
             { "source": "Ch21", "options": { "legend": "room", "color": "orange" } },
             { "source": "Ch16", "options": { "legend": "Baffle", "color": "cyan" } },
             { "source": "Ch15", "options": { "legend": "TPCTop", "color": "black" } },
+            { "source": "Ch20", "options": { "legend": "FECHeater", "color": "pink" } },
             { "source": "Ch14", "options": { "legend": "FEC", "color": "brown" } },
             { "source": "Ch13", "options": { "legend": "Anode", "color": "green" } },
             { "source": "Ch12", "options": { "legend": "SiPM", "color": "blue" } },
             { "source": "Ch11", "options": { "legend": "Bottom", "color": "red" } },
           ],
-          "options": { "xWidth": 10000, "yRange": [-200, 30], "frame": { "height": 270, "width": 480 } }
+          "options": { "xWidth": 3600, "yRange": [-200, 30], "frame": { "height": 270, "width": 480 } }
         },
         "LAr_Level": {
           "type": "trend-graph",
           "group": [
             { "source": "Ch4", "options": { "legend": "level", "color": "red" } },
           ],
-          "options": { "xWidth": 10000, "yRange": [-1, 70], "frame": { "height": 270, "width": 480 } }
+          "options": { "xWidth": 3600, "yRange": [-1, 70], "frame": { "height": 270, "width": 480 } }
         },
       }
     },
@@ -91,14 +94,14 @@ HSQuickLook.main.schema =
           "group": [
             { "source": "Ch1", "conversion": conversion_PKR251, "options": { "legend": "Outer", "color": "red" } },
           ],
-          "options": { "xWidth": 10000, "yRange": [0, 0.05], "frame": { "height": 270, "width": 480 } }
+          "options": { "xWidth": 3600, "yRange": [0, 0.05], "frame": { "height": 270, "width": 480 } }
         },
         "Inner_Pressure": {
           "type": "trend-graph",
           "group": [
             { "source": "Ch3", "options": { "legend": "Inner", "color": "green" } },
           ],
-          "options": { "xWidth": 10000, "yRange": [0.5, 3.0], "frame": { "height": 270, "width": 480 } }
+          "options": { "xWidth": 3600, "yRange": [0.5, 3.0], "frame": { "height": 270, "width": 480 } }
         }
       }
 
@@ -140,12 +143,14 @@ var status_configuration = {
   "Ch3": { "safe_range": [0, 1.3], "warning_range": [0, 2.0] },
   "Ch4": { "safe_range": [-1, 20], "warning_range": [-1, 25] },
   "Ch5": { "safe_range": [20.6, 22], "warning_range": [20.3, 20.6] },
+  "Ch7": { "warning_range": [-1e6, -0.05] },
   "Ch11": { "LAr_temp": [-189, -185] },
   "Ch12": { "LAr_temp": [-189, -185] },
   "Ch13": { "LAr_temp": [-189, -185] },
   "Ch14": { "LAr_temp": [-189, -185] },
   "Ch15": { "LAr_temp": [-189, -185] },
   "Ch16": { "LAr_temp": [-189, -185] },
+  "Ch20": { "safe_range": [-1e+6, 50], "warning_range": [25, 50] },
   "tempareture_warning": { "safe_range": [5, 50], "warning_range": [0, 5] }
 };
 
@@ -164,5 +169,11 @@ function status_func(name, v) {
 function status_func_temp(name, v) {
   if ((status_configuration[name]["LAr_temp"][0] <= v) && (status_configuration[name]["LAr_temp"][1] >= v)) {
     return "temp";
+  }
+}
+
+function status_func_HV(name, v) {
+  if ((status_configuration[name]["warning_range"][0] <= v) && (status_configuration[name]["warning_range"][1] >= v)) {
+    return "warning";
   }
 }
--- hsquicklook/user_data/schema/Short_Term_Trend.js
+++ hsquicklook/user_data/schema/Short_Term_Trend.js
@@ -15,6 +15,7 @@ HSQuickLook.main.schema =
         "Outer_Vacuum": { "source": "Ch1", "conversion": conversion_PKR251, "type": "float", "format": "%-5.2e Pa", "status": function (v) { return status_func("Ch1", v); } },
         "Inner_Pressure": { "source": "Ch3", "type": "float", "format": "%.3f Bar", "status": function (v) { return status_func("Ch3", v); } },
         "LAr_Level": { "source": "Ch4", "type": "float", "format": "%.2f cm", "status": function (v) { return status_func("Ch4", v); } },
+        "TPC_HV": { "source": "Ch7", "type": "float", "format": "%.3f kV", "status": function (v) { return status_func_HV("Ch7", v); }},
         "Oxygen": { "source": "Ch5", "conversion": conversion_OX600, "type": "float", "format": "%-5.1f &#037;", "status": function (v) { return status_func("Ch5", v); } },
         "Room_Tempareture": { "source": "Ch21", "type": "float", "format": "%.3f &#8451", },
         "Humidity": { "source": "Ch22", "type": "float", "format": "%.3f ％" },
@@ -32,6 +33,7 @@ HSQuickLook.main.schema =
       "contents": {
         "Top_Baffle": { "source": "Ch16", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch16", v); } },
         "TPC_Baffle": { "source": "Ch15", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch15", v); } },
+        "FEC_Heater": { "source": "Ch20", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func("Ch20", v); } },
         "Above_Anode": { "source": "Ch14", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch14", v); } },
         "Below_Anode": { "source": "Ch13", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch13", v); } },
         "SiPM": { "source": "Ch12", "type": "float", "format": "%.3f &#8451;", "status": function (v) { return status_func_temp("Ch12", v); } },
@@ -62,6 +64,7 @@ HSQuickLook.main.schema =
             { "source": "Ch21", "options": { "legend": "room", "color": "orange" } },
             { "source": "Ch16", "options": { "legend": "Baffle", "color": "cyan" } },
             { "source": "Ch15", "options": { "legend": "TPCTop", "color": "black" } },
+            { "source": "Ch20", "options": { "legend": "FECHeater", "color": "pink" } },
             { "source": "Ch14", "options": { "legend": "FEC", "color": "brown" } },
             { "source": "Ch13", "options": { "legend": "Anode", "color": "green" } },
             { "source": "Ch12", "options": { "legend": "SiPM", "color": "blue" } },
@@ -140,12 +143,14 @@ var status_configuration = {
   "Ch3": { "safe_range": [0, 1.3], "warning_range": [0, 2.0] },
   "Ch4": { "safe_range": [-1, 20], "warning_range": [-1, 25] },
   "Ch5": { "safe_range": [20.6, 22], "warning_range": [20.3, 20.6] },
-  "Ch11": { "LAr_temp": [-189, -185] },
-  "Ch12": { "LAr_temp": [-189, -185] },
-  "Ch13": { "LAr_temp": [-189, -185] },
-  "Ch14": { "LAr_temp": [-189, -185] },
-  "Ch15": { "LAr_temp": [-189, -185] },
-  "Ch16": { "LAr_temp": [-189, -185]},
+  "Ch7": { "warning_range": [-1e6, -0.05] },
+  "Ch11": { "LAr_temp": [-189, -183] },
+  "Ch12": { "LAr_temp": [-189, -183] },
+  "Ch13": { "LAr_temp": [-189, -183] },
+  "Ch14": { "LAr_temp": [-189, -183] },
+  "Ch15": { "LAr_temp": [-189, -183] },
+  "Ch16": { "LAr_temp": [-189, -183] },
+  "Ch20": { "safe_range": [-1e+6, 50], "warning_range": [25, 50] },
   "tempareture_warning": { "safe_range": [5, 50], "warning_range": [0, 5] }
 };
 
@@ -164,5 +169,11 @@ function status_func(name, v) {
 function status_func_temp(name, v) {
   if ((status_configuration[name]["LAr_temp"][0] <= v) && (status_configuration[name]["LAr_temp"][1] >= v)) {
     return "temp";
+  }
+}
+
+function status_func_HV(name, v) {
+  if ((status_configuration[name]["warning_range"][0] <= v) && (status_configuration[name]["warning_range"][1] >= v)) {
+    return "warning";
   }
 }
