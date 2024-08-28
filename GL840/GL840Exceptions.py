c24dea3efdfab47615f2a1cd0ed0a6054685b814
ShotaArai <93177954+STA205233@users.noreply.github.com>
2024-08-28 12:14:37 +0900

    Feature/warning (#14)
    * warning mode added
    
    * change regex expression
    
    * Merge remote-tracking branch 'origin/master' into feature/warning

---
+++ GL840/GL840Exceptions.py
@@ -0,0 +1,36 @@
+'''
+Exceptions
+-
+
+Exceptions for GL840.
+
+----------------
+Author: Shota Arai
+Date: 2024/08/28
+'''
+
+from .Warning import Warning as _Warning
+
+
+class GL840BaseException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(args)
+
+    def sound(self, frequency: list[float], second: list[float], volume: float, title: str | None = None, description: str | None = None) -> None:
+        title_ = title if title is not None else ""
+        description_ = "\n" + description if description is not None else ""
+        _Warning()(frequency, second, volume, "\n-------------" + title_ + f"-------------{description_}\nPress Ctrl+C to stop")
+
+
+class ChannelNotMatchError(GL840BaseException):
+    def __init__(self, description: str | None = None, title: str = "Channel Not Match Error", notify: bool = True) -> None:
+        super().__init__(title, description)
+        if notify:
+            super().sound([880, 0, 880, 0], [0.1, 0.05, 0.1, 0.2], 0.4, title, description)
+
+
+class GL840ConnectionError(GL840BaseException):
+    def __init__(self, description: str | None = None, title: str = "Connection Error", notify: bool = True) -> None:
+        super().__init__(title, description)
+        if notify:
+            super().sound([880, 0, 880, 0], [0.1, 0.05, 0.1, 0.2], 0.4, title, description)
---
+++ GL840/Warning.py
@@ -0,0 +1,61 @@
+"""
+Warning
+--
+
+Warning sound
+
+---------------------
+Author: Shota Arai
+Date: 2024/08/28
+"""
+
+SOUND_DEVICE = False
+try:
+    import sounddevice as sd
+    import numpy as np
+    SOUND_DEVICE = True
+    SAMPLE_RATE = 44100
+    sd.default.samplerate = SAMPLE_RATE
+
+except ImportError:
+    SOUND_DEVICE = False
+
+
+class Warning:
+    def __init__(self) -> None:
+        self.playing = False
+
+    def __call__(self, frequency: list[float], second: list[float], volume: float = 1, message: str | None = None) -> None:
+        if message is not None:
+            print(message)
+        if SOUND_DEVICE:
+            assert len(frequency) == len(second), "The length of frequency and second must be the same."
+            try:
+                total_sec = sum(second)
+                t = np.linspace(0, total_sec, int(total_sec * SAMPLE_RATE))
+                data = np.zeros_like(t)
+                i = 0
+                for j in range(len(frequency)):
+                    for tt in range(int(second[j] * SAMPLE_RATE)):
+                        assert i < total_sec * SAMPLE_RATE, "The length of t is not enough."
+                        data[i] = volume * np.sin(2 * np.pi * frequency[j] * t[i])
+                        i += 1
+                sd.play(data, loop=True, blocking=True)
+                self.playing = True
+            except Exception:
+                self.playing = False
+
+    def stop(self) -> None:
+        if SOUND_DEVICE and self.playing:
+            sd.stop()
+            self.playing = False
+
+    def __del__(self) -> None:
+        if SOUND_DEVICE and self.playing:
+            sd.stop()
+            self.playing = False
+
+
+if __name__ == "__main__":
+    w = Warning()
+    w([880, 440], [0.1, 0.1], 1, "This is a test.")
